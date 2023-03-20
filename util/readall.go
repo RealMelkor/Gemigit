e5d9416c368a064cac6e4190748182e183bef418
Raw Monk <realmelkor@proton.me>
2023-03-20 15:29:50 -0400

    add readall function for compatibility with older go version

---
+++ util/readall.go
@@ -0,0 +1,12 @@
+// +build go1.12 && go1.13 && go1.14 && go1.15
+
+package util
+
+import (
+	"io/ioutil"
+	"io"
+)
+
+func ReadAll(r io.Reader) ([]byte, error) {
+	return ioutil.ReadAll(r)
+}
---
+++ util/readall_modern.go
@@ -0,0 +1,12 @@
+//go:build openbsd
+// +build !go1.12 && !go1.13 && !go1.14 && !go1.15
+
+package util
+
+import (
+	"io"
+)
+
+func ReadAll(r io.Reader) ([]byte, error) {
+	return io.ReadAll(r)
+}
